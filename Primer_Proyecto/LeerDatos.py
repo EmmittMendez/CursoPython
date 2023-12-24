5fe04857b7fc878573a8e8cb561edba60e0027c7
Emmitt Mendez <77180330+EmmittMendez@users.noreply.github.com>
2023-12-24 16:57:21 -0600

    agregamos las practicas del primer dia

---
+++ Primer_Proyecto/LeerDatos.py
@@ -0,0 +1,5 @@
+#Mostrar mensajes a usuario, leer el dato y mostrar el resultado
+print(input("Dime tu nombre "))
+print(input("¿En qué país vives? "))
+#Se pueden concatenar los imputs
+print(input("Escribe tu nombre: ") + " " + input("Escribe tu apellido: "))
---
+++ Primer_Proyecto/PrimerProyecto.py
@@ -0,0 +1,19 @@
+#Cosas Basicas de python
+
+#Impresiones basicas
+print ("Hola mundo")
+#Ponemos diferentes las comillas para que las reconozca dentro del texto
+print ('Estudiar con "Python Total" es super divertido')
+#Imprimimos el resultado de la suma de los 2 numeros
+print (500+55)
+
+#Strings
+print('Línea 1\nLínea 2\nLínea 3')
+#Segundo ejercicio
+print ("Separacion por tabulacion")
+print ("A\tB\tC")
+print ("D\tE\tF")
+print ("G\tH\tI")
+#La diagonal invertida ayuda a hacer un escape de los caracteres especiales
+print ("Barra Normal: /")
+print ("Barra Invertida: \\")
