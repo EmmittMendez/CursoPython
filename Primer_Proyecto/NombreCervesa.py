a9c271dd3f8b8e3f9b6dafd4b8f92388c49b5425
Emmitt Mendez <77180330+EmmittMendez@users.noreply.github.com>
2023-12-24 18:55:25 -0600

    agregamos los proyectos del dia 1

---
+++ Primer_Proyecto/NombreCervesa.py
@@ -0,0 +1,6 @@
+#Programa que le asigna el nombre de una cerveza en base a 2 preguntas
+palabra1 = input ("Dime un color: ")
+palabra2 = input ("Dime una fruta: ")
+
+print("El nombre de la cerveza seria:")
+print('"' + palabra1 + " " + palabra2 + '"')
