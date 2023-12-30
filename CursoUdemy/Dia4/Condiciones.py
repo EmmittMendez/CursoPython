bce9a7333245dcc37bcc19bdef7cb872755642ae
Emmitt Mendez <77180330+EmmittMendez@users.noreply.github.com>
2023-12-30 15:16:10 -0600

    agregamos al archivo Condiciones el ultimo ejercicio

--- CursoUdemy/Dia4/Condiciones.py
+++ CursoUdemy/Dia4/Condiciones.py
@@ -48,3 +48,16 @@ elif edad < 18:
     print("No puedes conducir aún. Debes tener 18 años y contar con una licencia")
 elif edad >= 18 and tiene_licencia == False:
     print("No puedes conducir. Necesitas contar con una licencia")
+    
+#Verificamos si los candidatos a un trabajo cumplen con los requisitos
+habla_ingles = True
+sabe_python = False
+
+if habla_ingles == True and sabe_python == True:
+    print("Cumples con los requisitos para postularte")
+elif habla_ingles == False and sabe_python == False:
+    print("Para postularte, necesitas saber programar en Python y tener conocimientos de inglés")
+elif habla_ingles == False:
+    print("Para postularte, necesitas tener conocimientos de inglés")
+elif sabe_python== False: 
+    print("Para postularte, necesitas saber programar en Python")
